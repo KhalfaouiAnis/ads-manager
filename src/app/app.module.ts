5ba5bc1e41ebc4cd95455b628facadb3cfcdb633
KhalfaouiAnis <aniskhalfaoui9@gmail.com>
2023-02-11 11:40:25 +0100

    add bg image and fix login input styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,9 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { LoginPipe } from './login.pipe';
 
 @NgModule({
-  declarations: [AppComponent, LoginPipe],
+  declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
